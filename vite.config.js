5626887a725f206302476d64914605514bfc4815
Augusto <augusto.rocha004@gmail.com>
2024-11-14 19:56:05 -0300

    Primeiro commit no novo repositório

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/energia-renovavel/',  // Substitua pelo nome do seu repositório
-});
+  // Aqui você define o caminho base onde seu projeto vai ser hospedado
+  base: '/energia-renovavel/', // Substitua <nome-do-repositorio> pelo nome do seu repositório no GitHub
+
+  plugins: [
+    // Aqui você pode adicionar plugins como o React plugin, se necessário
+    // No caso do Vite com React, use o plugin @vitejs/plugin-react
+    require('@vitejs/plugin-react')()
+  ]
+})
